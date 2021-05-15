6a10f11a16e5b3a87c7f30eef1ccd49d8d834f62
Maikon Takeshi Tsujiguchi <maikon_numb@hotmail.com>
2021-05-15 18:47:11 -0300

    Adicionado solução e projeto base

---
+++ Estudo_de_LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estudo_de_LINQ
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
